91beb7996e8486f34e3c2cfa7f2d6b743b329111
chrisitian.sloper@gmail.com <Christian Sloper>
2020-03-21 12:59:38 +0100

    insert sql works

--- myCoronaCases/__init__.py
+++ myCoronaCases/__init__.py
@@ -1,10 +1,29 @@
 import logging
-
+import os
 import azure.functions as func
+import pyodbc
 
+
+def get_cursor_and_connection():
+    cnxn = pyodbc.connect(os.environ.get('DATABASECONNECTIONSTRING'))
+    # Create a cursor from the connection
+    cursor = cnxn.cursor()
+
+    return cursor, cnxn
+
+def insert_values_db(nor_cases, nor_dead, world_cases, world_dead):
+
+    crsr, _ = get_cursor_and_connection()
+
+    crsr.execute('insert into corona values (GETDATE(),?,?,?,?)', nor_cases, nor_dead, world_cases, world_dead)
+    crsr.commit()
+    crsr.close()
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+
+    insert_values_db(1,2,3,4)
 
     name = req.params.get('name')
     if not name:
