f2b1218010c7f4c45cb18e33af7e1d6aa7cbf712
Christian Sloper <christian.sloper@gmail.com>
2020-10-13 10:16:16 +0200

    better get tick

--- finansakrobot/__init__.py
+++ finansakrobot/__init__.py
@@ -6,7 +6,15 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    ticker = req.get('text')
+    ticker = req.params.get('text')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('text')
+
 
     if "." not in ticker:
         ticker += ".OL"
