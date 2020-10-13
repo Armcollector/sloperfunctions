6b727ea61457ccbd384d8a93ad0995cea5771d28
Christian Sloper <christian.sloper@gmail.com>
2020-10-13 19:39:07 +0200

    working

--- finansakrobot/__init__.py
+++ finansakrobot/__init__.py
@@ -18,7 +18,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     session = FuturesSession()
     # first request is started in background
     url = os.environ["SendMessageFunctionUrl"]
-    future_one = session.get(f"{url}?ticker={ticker}&channel={req.form["channel_name"]}")
+    future_one = session.get(
+        f"{url}?ticker={ticker}&channel={req.form['channel_name']}"
+    )
 
     logging.info("returning")
     return func.HttpResponse(
