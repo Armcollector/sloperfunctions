468c9e3679f8436a5dfb3b03220f41dfb9483eda
Christian Sloper <christian.sloper@gmail.com>
2020-10-13 11:31:31 +0200

    reading form

--- finansakrobot/__init__.py
+++ finansakrobot/__init__.py
@@ -7,23 +7,14 @@ import yfinance as yf
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    ticker = req.params.get('text')
-    if not ticker:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            ticker = req_body.get('text')
-
-
+    ticker = req._HttpRequest__form["text"]
+    
     if "." not in ticker:
         ticker += ".OL"
-    tick_dict = yf.Ticker(ticker)
+    tick_dict = yf.Ticker(ticker).info
 
 
     return func.HttpResponse(
             f"{tick_dict['shortName']} : {tick_dict['regularMarketPrice']}",
             status_code=200
     )
-
