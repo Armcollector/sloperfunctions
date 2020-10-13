b7d12fdc3dc162f6b40430ad7c6f001ab341a11a
Christian Sloper <christian.sloper@gmail.com>
2020-10-13 10:13:11 +0200

    using yfinance

--- finansakrobot/__init__.py
+++ finansakrobot/__init__.py
@@ -6,7 +6,17 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    ticker = req.get('text')
+
+    if "." not in ticker:
+        ticker += ".OL"
+    tick_dict = yf.Ticker(ticker)
+
+
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully.",
+            f"{tick_dict["shortName"]} : {tick_dict["regularMarketPrice"]}",
             status_code=200
     )
+
+
+#'token=KwFR72oXnE603fNS5vkB2Iv7&team_id=T0BBY7ATH&team_domain=sonatconsulting&channel_id=D4JEYNQGM&channel_name=directmessage&user_id=U4K3A5BHV&user_name=christiansloper&command=%2Ffinansakrobot&text=test&api_app_id=A01CC799WR3&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT0BBY7ATH%2F1416704205270%2FOlIxr9OxTCzOZ5d70WfKicGn&trigger_id=1447277879568.11406248935.1fed91f59bcc2d5c0255dde3cc6dcf53''
